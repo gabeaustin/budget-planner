63518e5f12ae104bdf7bf564a86e0c9c70bf52e9
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-18 19:59:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Budget from "./components/Budget";
+
+const App = () => {
+    return(
+        <div className="container">
+            <h1 className="mt-3">
+                My Budget Planner
+            </h1>
+
+            <div className="row mt-3">
+                <div className="col-sm">
+                    <Budget />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
